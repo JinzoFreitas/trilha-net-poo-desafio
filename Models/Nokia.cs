a0f4b2f4b7a85052abd96a31246e997d36e10bbe
Janderson S. Freitas <95733951+JinzoFreitas@users.noreply.github.com>
2024-01-08 22:02:07 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // TODO: Herdar da classe "Smartphone" - OK
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo" - OK
+        public void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia")
+        }
     }
 }
